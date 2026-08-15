722e4b98a8499c7200858190c1dafa5f9828af0e
Dhon <dhon20640@gmail.com>
2026-08-15 12:44:40 -0300

    Update loader URL to use loadstring

--- volt.lua
+++ volt.lua
@@ -1,1 +1,1 @@
-https://api.luarmor.net/files/v4/loaders/b79bf9b48f9f855ecadaf879d3a3c79c.lua
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/b79bf9b48f9f855ecadaf879d3a3c79c.lua"))()
